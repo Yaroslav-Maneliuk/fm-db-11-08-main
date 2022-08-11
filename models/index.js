5405606e50ad7223a04ce754ac255c6a33eb7d39
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-08-11 12:06:44 +0300

    Initial commit

---
+++ models/index.js
@@ -0,0 +1,16 @@
+const { Client } = require('pg');
+const config = require('../configs/db.json');
+
+const User = require('./User');
+const Phone = require('./Phone');
+
+const client = new Client(config);
+
+User._client = client;
+Phone._client = client;
+
+module.exports = {
+  client,
+  User,
+  Phone
+};
